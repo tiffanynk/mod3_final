19b8ff64da89a43dc132a1efe696e220b93d8df4
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-11-10 16:34:16 -0700

    updated variable names

--- full-snack-frontend/index.js
+++ full-snack-frontend/index.js
@@ -25,6 +25,7 @@ function createBoard() {
         square.className = 'square' 
         square.setAttribute('draggable', true)
         square.setAttribute('id', i)
+        scoreBox.textContent = 0
         let randomSpook = Math.floor(Math.random() * spookyIcons.length)
         square.style.backgroundImage = spookyIcons[randomSpook]
         grid.appendChild(square)
@@ -69,7 +70,6 @@ function dragDrop() {
     squareIdBeingReplaced = parseInt(this.id)
     this.style.backgroundImage = spookBeingDragged
     squares[squareIdBeingDragged].style.backgroundImage = spookBeingReplaced
-    console.log('dragDrop')
 }
 
 function dragEnd() {
@@ -85,16 +85,19 @@ function dragEnd() {
 
     if (squareIdBeingReplaced && validMove) {
         squareIdBeingReplaced = null
+        console.log('squareIdBeingReplaced && is validMove')
     } else if (squareIdBeingReplaced && !validMove) {
+        console.log('squareIdBeingReplaced && !validMove')
         squares[squareIdBeingReplaced].style.backgroundImage = spookBeingReplaced
         squares[squareIdBeingDragged].style.backgroundImage = spookBeingDragged
     } else {
+        console.log('else')
         squares[squareIdBeingDragged].style.backgroundImage = spookBeingDragged
     }
 }
 
 function dropDownIntoSquare() {
-    for (i = 0; i < 55; i ++) {
+    for (i = 0; i < 56; i++) {
         if (squares[i + width].style.backgroundImage === '') {
             squares[i + width].style.backgroundImage = squares[i].style.backgroundImage
             squares[i].style.backgroundImage = ''
@@ -104,14 +107,18 @@ function dropDownIntoSquare() {
             
             if (isFirstRow && (squares[i].style.backgroundImage === '')) {
                 let randomSpook = Math.floor(Math.random() * spookyIcons.length)
+                console.log('randomSpook', randomSpook)
                 squares[i].style.backgroundImage = spookyIcons[randomSpook]
+                console.log(`new icon url for  div ${squares[i]}`, squares[i].style.backgroundImage)
             }
         }
     }
 }
 
+dropDownIntoSquare()
+
 function checksRowFor4() {
-    for (i = 0; i < 60; i ++) {
+    for (i = 0; i < 60; i++) {
         let rowOfFour = [i, i + 1, i + 2, i + 3]
         let decidedSpook = squares[i].style.backgroundImage
         const isEmpty = squares[i].style.backgroundImage === ''
@@ -121,11 +128,11 @@ function checksRowFor4() {
         const invalidIndex = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55]
         if (invalidIndex.includes(i)) continue
 
-        if (rowOfFour.every(index => squares[index].style.backgroundImage === decidedSpook && !isEmpty)) {
+        if (rowOfFour.every(i => squares[i].style.backgroundImage === decidedSpook && !isEmpty)) {
             score += 4
             scoreBox.innerHTML = score
-            rowOfFour.forEach(index => {
-                squares[index].style.backgroundImage = ''
+            rowOfFour.forEach(i => {
+                squares[i].style.backgroundImage = ''
             })
         }
     }
@@ -133,17 +140,17 @@ function checksRowFor4() {
 checksRowFor4()
 
 function checksColumnFor4() {
-    for (i = 0; i < 39; i ++) {
+    for (i = 0; i < 39; i++) {
         let columnOfFour = [i, i + width, i + width * 2, i + width * 3]
         let decidedSpook = squares[i].style.backgroundImage
         const isEmpty = squares[i].style.backgroundImage === ''
 
-        if (columnOfFour.every(index => squares[index].style.backgroundImage === decidedSpook && !isEmpty)) {
+        if (columnOfFour.every(i => squares[i].style.backgroundImage === decidedSpook && !isEmpty)) {
             score += 4
             console.log('column of 4', score)
             scoreBox.innerHTML = score
-            columnOfFour.forEach(index => {
-                squares[index].style.backgroundImage = ''
+            columnOfFour.forEach(i => {
+                squares[i].style.backgroundImage = ''
             })
         }
     }
@@ -151,20 +158,19 @@ function checksColumnFor4() {
 checksColumnFor4()
 
 function checksRowFor3() {
-    for (i = 0; i < 61; i ++) {
-        let rowOfThree = [i, i+1, i+2]
+    for (i = 0; i < 61; i++) {
+        let rowOfThree = [i, i + 1, i + 2]
         let decidedSpook = squares[i].style.backgroundImage
         const isEmpty = squares[i].style.backgroundImage === ''
 
         const invalidIndex = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
         if (invalidIndex.includes(i)) continue
 
-        if(rowOfThree.every(index => squares[index].style.backgroundImage === decidedSpook && !isEmpty)) {
+        if(rowOfThree.every(i => squares[i].style.backgroundImage === decidedSpook && !isEmpty)) {
             score += 3
-            console.log('row of 3', score)
             scoreBox.innerHTML = score
-            rowOfThree.forEach(index => {
-                squares[index].style.backgroundImage = ''
+            rowOfThree.forEach(i => {
+                squares[i].style.backgroundImage = ''
             })
         }
     }
@@ -173,17 +179,16 @@ function checksRowFor3() {
 checksRowFor3()
 
 function checksColumnFor3() {
-    for (i = 0; i < 47; i ++) {
+    for (i = 0; i < 47; i++) {
         let columnOfThree = [i, i + width, i + width * 2]
         let decidedSpook = squares[i].style.backgroundImage
         const isEmpty = squares[i].style.backgroundImage === ''
 
-        if (columnOfThree.every(index => squares[index].style.backgroundImage === decidedSpook && !isEmpty)) {
+        if (columnOfThree.every(i => squares[i].style.backgroundImage === decidedSpook && !isEmpty)) {
             score += 3
-            console.log('column of 3', score)
             scoreBox.innerHTML = score
-            columnOfThree.forEach(index => {
-                squares[index].style.backgroundImage = ''
+            columnOfThree.forEach(i => {
+                squares[i].style.backgroundImage = ''
             })
         }
     }
