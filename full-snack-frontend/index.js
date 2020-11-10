8edb824b57152bc09084051a232063377ed4ed2c
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-11-10 14:40:40 -0700

    game code mostly functioning

--- full-snack-frontend/index.js
+++ full-snack-frontend/index.js
@@ -50,25 +50,18 @@ squares.forEach(square => square.addEventListener('drop', dragDrop))
 function dragStart(){
     spookBeingDragged = this.style.backgroundImage
     squareIdBeingDragged = parseInt(this.id)
-    console.log('dragStart')
-
 }
 
-function dragOver(e) {
-    e.preventDefault()
-    console.log('dragOver')
+function dragOver(event) {
+    event.preventDefault()
 }
 
-function dragEnter(e) {
-    e.preventDefault()
-    console.log('dragEnter')
-
+function dragEnter(event) {
+    event.preventDefault()
 }
 
 function dragLeave() {
     // this.style.backgroundImage = ''
-    console.log('dragLeave')
-
 }
 
 function dragDrop() {
@@ -80,29 +73,38 @@ function dragDrop() {
 }
 
 function dragEnd() {
-    console.log('dragEnd')
+    //defining moves so that icons can only move one space from its current position
+    let validMoves = [
+        squareIdBeingDragged - 1 , 
+        squareIdBeingDragged - width, 
+        squareIdBeingDragged + 1, 
+        squareIdBeingDragged + width
+    ]
 
-    let validMoves = [squareIdBeingDragged - 1 , squareIdBeingDragged - width, squareIdBeingDragged + 1, squareIdBeingDragged + width]
     let validMove = validMoves.includes(squareIdBeingReplaced)
 
     if (squareIdBeingReplaced && validMove) {
         squareIdBeingReplaced = null
-    }  else if (squareIdBeingReplaced && !validMove) {
-       squares[squareIdBeingReplaced].style.backgroundImage = spookBeingReplaced
-       squares[squareIdBeingDragged].style.backgroundImage = spookBeingDragged
-    } else  squares[squareIdBeingDragged].style.backgroundImage = spookBeingDragged
+    } else if (squareIdBeingReplaced && !validMove) {
+        squares[squareIdBeingReplaced].style.backgroundImage = spookBeingReplaced
+        squares[squareIdBeingDragged].style.backgroundImage = spookBeingDragged
+    } else {
+        squares[squareIdBeingDragged].style.backgroundImage = spookBeingDragged
+    }
 }
 
 function dropDownIntoSquare() {
     for (i = 0; i < 55; i ++) {
-        if(squares[i + width].style.backgroundImage === '') {
+        if (squares[i + width].style.backgroundImage === '') {
             squares[i + width].style.backgroundImage = squares[i].style.backgroundImage
             squares[i].style.backgroundImage = ''
+            
             const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
             const isFirstRow = firstRow.includes(i)
+            
             if (isFirstRow && (squares[i].style.backgroundImage === '')) {
-              let randomSpook = Math.floor(Math.random() * spookyIcons.length)
-              squares[i].style.backgroundImage = spookyIcons[randomSpook]
+                let randomSpook = Math.floor(Math.random() * spookyIcons.length)
+                squares[i].style.backgroundImage = spookyIcons[randomSpook]
             }
         }
     }
@@ -119,7 +121,6 @@ function checksRowFor4() {
 
         if (rowOfFour.every(index => squares[index].style.backgroundImage === decidedSpook && !isEmpty)) {
             score += 4
-            console.log('row of 4', score)
             scoreBox.innerHTML = score
             rowOfFour.forEach(index => {
                 squares[index].style.backgroundImage = ''
@@ -175,7 +176,7 @@ function checksColumnFor3() {
         let decidedSpook = squares[i].style.backgroundImage
         const isEmpty = squares[i].style.backgroundImage === ''
 
-        if(columnOfThree.every(index => squares[index].style.backgroundImage === decidedSpook && !isEmpty)) {
+        if (columnOfThree.every(index => squares[index].style.backgroundImage === decidedSpook && !isEmpty)) {
             score += 3
             console.log('column of 3', score)
             scoreBox.innerHTML = score
@@ -188,7 +189,7 @@ function checksColumnFor3() {
 
 checksColumnFor3()
 
-window.setInterval(function(){
+window.setInterval(function() {
     checksRowFor4()
     checksColumnFor4()
     checksRowFor3()
