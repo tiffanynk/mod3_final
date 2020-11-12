d53caf1965404336135c1f1595bfe553d5bd1054
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-11-11 18:50:24 -0700

    fixed empty drop issue

--- full-snack-frontend/index.js
+++ full-snack-frontend/index.js
@@ -3,10 +3,10 @@ const usersURL = `${baseURL}/users`;
 const loginURL = `${baseURL}/login`;
 
 const loginLink = document.querySelector('.login-link');
-const grid = document.querySelector('.grid');
-const scoreBox = document.getElementById('score');
+const board = document.querySelector('.board');
+const scoreboard = document.getElementById('score');
 const width = 8;
-let squares = [];
+let cells = [];
 let score = 0;
 
 const spookyIcons = [
@@ -21,15 +21,15 @@ const spookyIcons = [
 //creates gameboard
 function createBoard() {
     for (let i=0; i < width*width; i++) {
-        const square = document.createElement('div')
-        square.className = 'square' 
-        square.setAttribute('draggable', true)
-        square.setAttribute('id', i)
-        scoreBox.textContent = 0
+        const cell = document.createElement('div')
+        cell.className = 'cell' 
+        cell.setAttribute('draggable', true)
+        cell.setAttribute('id', i)
+        scoreboard.textContent = 0
         let randomSpook = Math.floor(Math.random() * spookyIcons.length)
-        square.style.backgroundImage = spookyIcons[randomSpook]
-        grid.appendChild(square)
-        squares.push(square)
+        cell.style.backgroundImage = spookyIcons[randomSpook]
+        board.appendChild(cell)
+        cells.push(cell)
     }
 }
 
@@ -38,19 +38,18 @@ createBoard()
 // Dragging the Spooks
 let spookBeingDragged
 let spookBeingReplaced
-let squareIdBeingDragged
-let squareIdBeingReplaced
+let cellIdBeingDragged
+let cellIdBeingReplaced
 
-squares.forEach(square => square.addEventListener('dragstart', dragStart))
-squares.forEach(square => square.addEventListener('dragend', dragEnd))
-squares.forEach(square => square.addEventListener('dragover', dragOver))
-squares.forEach(square => square.addEventListener('dragenter', dragEnter))
-squares.forEach(square => square.addEventListener('dragleave', dragLeave))
-squares.forEach(square => square.addEventListener('drop', dragDrop))
+cells.forEach(cell => cell.addEventListener('dragstart', dragStart))
+cells.forEach(cell => cell.addEventListener('dragend', dragEnd))
+cells.forEach(cell => cell.addEventListener('dragover', dragOver))
+cells.forEach(cell => cell.addEventListener('dragenter', dragEnter))
+cells.forEach(cell => cell.addEventListener('drop', dragDrop))
 
 function dragStart(){
     spookBeingDragged = this.style.backgroundImage
-    squareIdBeingDragged = parseInt(this.id)
+    cellIdBeingDragged = parseInt(this.id)
 }
 
 function dragOver(event) {
@@ -61,145 +60,129 @@ function dragEnter(event) {
     event.preventDefault()
 }
 
-function dragLeave() {
-    // this.style.backgroundImage = ''
-}
-
 function dragDrop() {
     spookBeingReplaced = this.style.backgroundImage
-    squareIdBeingReplaced = parseInt(this.id)
+    cellIdBeingReplaced = parseInt(this.id)
     this.style.backgroundImage = spookBeingDragged
-    squares[squareIdBeingDragged].style.backgroundImage = spookBeingReplaced
+    cells[cellIdBeingDragged].style.backgroundImage = spookBeingReplaced
 }
 
 function dragEnd() {
     //defining moves so that icons can only move one space from its current position
     let validMoves = [
-        squareIdBeingDragged - 1 , 
-        squareIdBeingDragged - width, 
-        squareIdBeingDragged + 1, 
-        squareIdBeingDragged + width
+        cellIdBeingDragged - 1 , 
+        cellIdBeingDragged - width, 
+        cellIdBeingDragged + 1, 
+        cellIdBeingDragged + width
     ]
 
-    let validMove = validMoves.includes(squareIdBeingReplaced)
+    let validMove = validMoves.includes(cellIdBeingReplaced)
 
-    if (squareIdBeingReplaced && validMove) {
-        squareIdBeingReplaced = null
-        console.log('squareIdBeingReplaced && is validMove')
-    } else if (squareIdBeingReplaced && !validMove) {
-        console.log('squareIdBeingReplaced && !validMove')
-        squares[squareIdBeingReplaced].style.backgroundImage = spookBeingReplaced
-        squares[squareIdBeingDragged].style.backgroundImage = spookBeingDragged
+    if (cellIdBeingReplaced && validMove) {
+        cellIdBeingReplaced = null
+    } else if (cellIdBeingReplaced && !validMove) {
+        cells[cellIdBeingReplaced].style.backgroundImage = spookBeingReplaced
+        cells[cellIdBeingDragged].style.backgroundImage = spookBeingDragged
     } else {
-        console.log('else')
-        squares[squareIdBeingDragged].style.backgroundImage = spookBeingDragged
+        cells[cellIdBeingDragged].style.backgroundImage = spookBeingDragged
     }
 }
 
-function dropDownIntoSquare() {
+function dropSpooks() {
     for (i = 0; i < 56; i++) {
-        if (squares[i + width].style.backgroundImage === '') {
-            squares[i + width].style.backgroundImage = squares[i].style.backgroundImage
-            squares[i].style.backgroundImage = ''
+        if (cells[i + width].style.backgroundImage === '') {
             
-            const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
-            const isFirstRow = firstRow.includes(i)
+            cells[i + width].style.backgroundImage = cells[i].style.backgroundImage
+            cells[i].style.backgroundImage = ''
             
-            if (isFirstRow && (squares[i].style.backgroundImage === '')) {
-                let randomSpook = Math.floor(Math.random() * spookyIcons.length)
-                console.log('randomSpook', randomSpook)
-                squares[i].style.backgroundImage = spookyIcons[randomSpook]
-                console.log(`new icon url for  div ${squares[i]}`, squares[i].style.backgroundImage)
-            }
+        }
+        const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
+        const isFirstRow = firstRow.includes(i)
+
+        if (isFirstRow && cells[i].style.backgroundImage === '') {
+
+            let randomSpook = Math.floor(Math.random() * spookyIcons.length)
+            cells[i].style.backgroundImage = spookyIcons[randomSpook]
         }
     }
 }
 
-dropDownIntoSquare()
-
 function checksRowFor4() {
     for (i = 0; i < 60; i++) {
         let rowOfFour = [i, i + 1, i + 2, i + 3]
-        let decidedSpook = squares[i].style.backgroundImage
-        const isEmpty = squares[i].style.backgroundImage === ''
+        let decidedSpook = cells[i].style.backgroundImage
+        const isEmpty = cells[i].style.backgroundImage === ''
 
         //array of all indexes that are not valid, i don't want my row of 3 to start on any of these indicies
         //fixes matches when a row starts at left or side or if half of its body is at halfway point
         const invalidIndex = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55]
         if (invalidIndex.includes(i)) continue
 
-        if (rowOfFour.every(i => squares[i].style.backgroundImage === decidedSpook && !isEmpty)) {
+        if (rowOfFour.every(i => cells[i].style.backgroundImage === decidedSpook && !isEmpty)) {
             score += 4
-            scoreBox.innerHTML = score
+            scoreboard.innerHTML = score
             rowOfFour.forEach(i => {
-                squares[i].style.backgroundImage = ''
+                cells[i].style.backgroundImage = ''
             })
         }
     }
 }
-checksRowFor4()
 
 function checksColumnFor4() {
     for (i = 0; i < 39; i++) {
         let columnOfFour = [i, i + width, i + width * 2, i + width * 3]
-        let decidedSpook = squares[i].style.backgroundImage
-        const isEmpty = squares[i].style.backgroundImage === ''
+        let decidedSpook = cells[i].style.backgroundImage
+        const isEmpty = cells[i].style.backgroundImage === ''
 
-        if (columnOfFour.every(i => squares[i].style.backgroundImage === decidedSpook && !isEmpty)) {
+        if (columnOfFour.every(i => cells[i].style.backgroundImage === decidedSpook && !isEmpty)) {
             score += 4
-            console.log('column of 4', score)
-            scoreBox.innerHTML = score
+            scoreboard.innerHTML = score
             columnOfFour.forEach(i => {
-                squares[i].style.backgroundImage = ''
+                cells[i].style.backgroundImage = ''
             })
         }
     }
 }
-checksColumnFor4()
 
 function checksRowFor3() {
     for (i = 0; i < 61; i++) {
         let rowOfThree = [i, i + 1, i + 2]
-        let decidedSpook = squares[i].style.backgroundImage
-        const isEmpty = squares[i].style.backgroundImage === ''
+        let decidedSpook = cells[i].style.backgroundImage
+        const isEmpty = cells[i].style.backgroundImage === ''
 
         const invalidIndex = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
         if (invalidIndex.includes(i)) continue
 
-        if(rowOfThree.every(i => squares[i].style.backgroundImage === decidedSpook && !isEmpty)) {
+        if(rowOfThree.every(i => cells[i].style.backgroundImage === decidedSpook && !isEmpty)) {
             score += 3
-            scoreBox.innerHTML = score
+            scoreboard.innerHTML = score
             rowOfThree.forEach(i => {
-                squares[i].style.backgroundImage = ''
+                cells[i].style.backgroundImage = ''
             })
         }
     }
 }
-
-checksRowFor3()
 
 function checksColumnFor3() {
     for (i = 0; i < 47; i++) {
         let columnOfThree = [i, i + width, i + width * 2]
-        let decidedSpook = squares[i].style.backgroundImage
-        const isEmpty = squares[i].style.backgroundImage === ''
+        let decidedSpook = cells[i].style.backgroundImage
+        const isEmpty = cells[i].style.backgroundImage === ''
 
-        if (columnOfThree.every(i => squares[i].style.backgroundImage === decidedSpook && !isEmpty)) {
+        if (columnOfThree.every(i => cells[i].style.backgroundImage === decidedSpook && !isEmpty)) {
             score += 3
-            scoreBox.innerHTML = score
+            scoreboard.innerHTML = score
             columnOfThree.forEach(i => {
-                squares[i].style.backgroundImage = ''
+                cells[i].style.backgroundImage = ''
             })
         }
     }
 }
-
-checksColumnFor3()
 
 window.setInterval(function() {
     checksRowFor4()
     checksColumnFor4()
     checksRowFor3()
     checksColumnFor3()
-    dropDownIntoSquare()
+    dropSpooks()
   }, 100);
